f1c182e381edcf2d0aa44fdbe2e820438bdf4788
Zuhair Saleem <zuhairsaleem343@gmail.com>
2024-11-03 23:39:13 +0500

    added home and done routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'}, //used to render home component directly on app starting we can also do it by giving '' to next line
+  { path: 'home',component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
