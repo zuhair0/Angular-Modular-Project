f17deb2d7a8237e3eb8fa977a64565ca3be6887d
Zuhair Saleem <zuhairsaleem343@gmail.com>
2024-11-06 23:46:14 +0500

    added about component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'}, //used to render home component directly on app starting we can also do it by giving '' to next line
-  { path: 'home',component: HomeComponent}
+  { path: 'home',component: HomeComponent},
+  {path:'about', component:AboutComponent}
 ];
 
 @NgModule({
