333f4ad2aedd7a82be2d520f91e2c37d364a6a4d
macbookpro <zuhairsaleem123@gmail.com>
2024-12-08 22:40:42 +0500

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
     loadChildren: ()=> import('./components/about/about.module').then((m)=>m.AboutModule)
   }
 ];
-//hghglll
+//hghglllsas
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
