e20f3be13c5d341c407f4182c541627bef6fb0cf
macbookpro <zuhairsaleem123@gmail.com>
2024-12-08 22:56:07 +0500

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './components/about/about.component';
-//..
+//...
 const routes: Routes = [
   //{path: '', redirectTo: '/home', pathMatch: 'full'}, //used to render home component directly on app starting we can also do it by giving '' to next line
   // { path: 'home',component: HomeComponent},
